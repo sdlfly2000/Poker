3ca1e24826c84274e1feb9f4e4e8d12f6c587ee9
Jay Shi <JayShi@eurofins.com>
2020-10-11 14:39:08 +0800

    https://github.com/sdlfly2000/Poker/projects/1#card-47133325

---
+++ Poker.UnitTests/Cache/SessionCacheTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.DependencyInjection;
+using Poker.Cache;
+
+namespace Poker.UnitTests.Cache
+{
+    [TestClass]
+    public class SessionCacheTest
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void SessionCache_Get_SessionCache_Instance()
+        {
+            string[] args = { "%LAUNCHER_ARGS%" };
+
+            // Arrange
+            var host = Program.CreateHostBuilder(args).Build();
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                // Act
+                var sessionCache = services.GetRequiredService<ISessionCache>();
+
+                // Assert
+                Assert.IsNotNull(sessionCache);
+            }
+        }
+    }
+}
