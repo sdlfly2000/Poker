f9f2aa7ccd91c2a94ee9f1a01b03382f3c686227
Jay Shi <JayShi@eurofins.com>
2020-10-11 20:23:46 +0800

    https://github.com/sdlfly2000/Poker/projects/1#card-47035449

--- Poker.UnitTests/Controllers/VoteControllerTest.cs
+++ Poker.UnitTests/Controllers/VoteControllerTest.cs
@@ -9,6 +9,10 @@ using Poker.Controllers;
 
 namespace Poker.UnitTests.Controllers
 {
+    using System.Linq;
+
+    using Newtonsoft.Json;
+
     [TestClass]
     public class VoteControllerTest
     {
@@ -16,6 +20,7 @@ namespace Poker.UnitTests.Controllers
         private Guid _sessionId2 = Guid.NewGuid();
         private Guid _sessionIdNotExist = Guid.NewGuid();
 
+        private Mock<ISessionCache> _sessionCache;
         private VoteController _controller;
 
         [TestInitialize]
@@ -23,16 +28,22 @@ namespace Poker.UnitTests.Controllers
         {
             var sessionIds = new List<Guid> { _sessionId1, _sessionId2};
 
-            var sessionCache = new Mock<ISessionCache>();
-            sessionCache
+            _sessionCache = new Mock<ISessionCache>();
+            _sessionCache
                 .Setup(s => s.SetVote(It.IsAny<Guid>(), It.IsAny<Vote>()))
                 .Returns<Guid, Vote>((id, vote) => vote);
-            sessionCache
+            _sessionCache
                 .Setup(s => s.AddSessionId(It.IsAny<Guid>()))
                 .Returns<Guid>((id) => new List<Guid> { id });
-            sessionCache.Setup(s => s.GetAllSessionIds()).Returns(sessionIds);
+            _sessionCache.Setup(s => s.GetAllSessionIds()).Returns(sessionIds);
+            _sessionCache
+                .Setup(s => s.GetVote(It.IsAny<Guid>()))
+                .Returns<Guid>((id) => new Vote
+                               {
+                                   SessionId = id.ToString()
+                               });
 
-            _controller = new VoteController(sessionCache.Object);
+            _controller = new VoteController(_sessionCache.Object);
         }
 
         [TestMethod, TestCategory("UnitTest")]
@@ -84,6 +95,35 @@ namespace Poker.UnitTests.Controllers
             // Asserts
             Assert.IsNotNull(result);
             Assert.AreEqual(false, result.Value);
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void VoteController_GIVEN_WHEN_GetAllVotes_THEN_Votes()
+        {
+            // Arrange
+
+            // Act
+            var result = (OkObjectResult)_controller.GetAllVotes();
+            var votes = JsonConvert.DeserializeObject<IList<Vote>>((string)result.Value);
+
+            // Asserts
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(votes);
+            Assert.IsTrue(votes.Any(v => v.SessionId.Equals(_sessionId1.ToString())));
+            Assert.IsTrue(votes.Any(v => v.SessionId.Equals(_sessionId2.ToString())));
+        }
+
+        [TestMethod, TestCategory("UnitTest")]
+        public void VoteController_GIVEN_WHEN_GetAllVotes_THEN_Empty_Votes()
+        {
+            // Arrange
+            _sessionCache.Setup(s => s.GetAllSessionIds()).Returns((IList<Guid>)null);
+
+            // Act
+            var result = (NoContentResult)_controller.GetAllVotes();
+
+            // Asserts
+            Assert.IsNotNull(result);
         }
     }
 }
